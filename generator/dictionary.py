f42c1bb01366e1595bb191b13aa4a4056594fdef
Hyunjoon Cheon <hyunjooncheon@yonsei.ac.kr>
2021-10-13 12:19:34 +0900

    generator/dictionary: tag-based system

--- generator/dictionary.py
+++ generator/dictionary.py
@@ -1,32 +1,72 @@
-from typing import Set
+from typing import List, Set
 import functools
 import operator
+import json
+
+
+class Namespace():
+    pass
 
 
 class DictItem():
-    __slots__ = ('text', 'props')
-    def __init__(self, text: str, **kwargs):
+    __slots__ = ('text', 'props', 'tags')
+
+    def __init__(self, text: str, tags=[], **kwargs):
         self.text = text
+        self.tags = set(tags)
         self.props = kwargs
 
     def of(self, name):
         return self.props[name]
+
 
 class Dictionary():
     __slots__ = ('tokens', 'children_of')
     key_t = str
 
     def __init__(self):
-        self.tokens = dict()
+        self.tokens = list()
         self.children_of = dict()
 
-    def _add_key(self, key: key_t):
-        if key not in self.tokens:
-            self.tokens[key] = set()
+    def export(self, alias=None, **kwargs):
+        data = {
+            'alias': { k : v for k, v in alias.__dict__.items() if alias is not None },
+            'dictionary': {
+                'children_of': {
+                    k: list(v) for k, v in self.children_of.items()
+                },
+                'tokens': [
+                    {
+                        'text': x.text,
+                        'tags': list(x.tags),
+                        'props': x.props
+                    } for x in self.tokens
+                ]
+            }
+        }
+
+        return json.dumps(data, **kwargs)
+
+    @staticmethod
+    def load(s):
+        data = json.loads(s)
+
+        d = Dictionary()
+        a = Namespace()
+
+        d.children_of = { k: set(v) for k, v in data['dictionary']['children_of'].items() }
+        d.tokens = list( DictItem(x['text'], x['tags'], **x['props']) for x in data['dictionary']['tokens'] )
+        a.__dict__ = data['alias']
+
+        return d, a
 
     def add_token(self, key: key_t, value: DictItem):
-        self._add_key(key)
-        self.tokens[key].add(value)
+        value.tags.add(key)
+        self.tokens.append(value)
+
+    def add_token_tag(self, value: DictItem, *tags: List[str]):
+        value.tags |= set(tags)
+        self.tokens.append(value)
 
     def set_child_relation(self, parent: key_t, child: key_t):
         if parent not in self.children_of:
@@ -51,11 +91,8 @@ class Dictionary():
 
     def resolve_items(self, key) -> Set[DictItem]:
         keys = self.resolve_keys(key)
+        return set(filter(lambda x: keys & x.tags, self.tokens))
 
-        return functools.reduce(
-                operator.or_,
-                map(lambda k: self.tokens[k] if k in self.tokens else set(), keys),
-                set())
 
 # alias
 DictToken = DictItem
